82e40a4a043a78150754d0413f5ee380e15ff599
YanZinchenko <Zinchenko.yan@gmail.com>
2021-10-13 20:50:15 +0300

    creat Site and write variables

---
+++ js/script.js
@@ -0,0 +1,9 @@
+let chooseBtn = document.getElementById('choose'),
+    receiveBtn = document.getElementById('receive'),
+    heading = document.getElementsByTagName('h2')[0],
+    nameInput = document.getElementsByClassName('contactform_name')[0], //Its Old Methods
+    phoneInput = document.querySelector('.contactform_phone'),
+    mailInput = document.querySelectorAll('.contactform_mail')[0];
+
+
+   
